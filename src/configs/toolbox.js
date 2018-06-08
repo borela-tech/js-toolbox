d146daec18b48ea3d2c6eb761c159a820fdb71e3
Alexandre Borela <alexandre.borela@gmail.com>
2018-06-07 22:49:29 -0300

    Load settings from package and ctrine json

--- src/configs/toolbox.js
+++ src/configs/toolbox.js
@@ -10,6 +10,25 @@
 // License for the specific language governing permissions and limitations under
 // the License.
 
+import {camelizeKeys} from 'humps'
+import {existsSync} from 'fs'
+import {join} from 'path'
+import {PACKAGE_DIR} from '../paths'
+
+const PACKAGE_JSON_PATH = join(PACKAGE_DIR, 'package.json')
+const PACKAGE_JSON = existsSync(PACKAGE_JSON_PATH)
+  ? require(PACKAGE_JSON_PATH)
+  : {}
+
+const CTRINE_JSON_PATH = join(PACKAGE_DIR, 'ctrine.json')
+const CTRINE_JSON = existsSync(CTRINE_JSON_PATH)
+  ? require(CTRINE_JSON_PATH)
+  : {}
+
 export function getSettings() {
-  return process.env
+  return camelizeKeys({
+    ...PACKAGE_JSON.ctrine,
+    ...CTRINE_JSON,
+    ...process.env,
+  })
 }
