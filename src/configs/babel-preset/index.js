f3ac9a5b51303a4dcb73f59fe10b2394c25f3483
Alexandre Borela <alexandre.borela@gmail.com>
2018-07-20 21:41:01 -0300

    Fix babel config

--- src/configs/babel-preset/index.js
+++ src/configs/babel-preset/index.js
@@ -29,7 +29,8 @@ module.exports = function () {
   let result = {
     plugins: [],
     presets: [[getModulePath('@babel/preset-env'), {
-      targets: {browsers, node}
+      ...browsers && {browsers},
+      ...node && {node}
     }]],
   }
 
