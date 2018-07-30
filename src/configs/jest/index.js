02355e380cba03b02d21dde55f74a9e541ef7143
Alexandre Borela <alexandre.borela@gmail.com>
2018-07-30 11:43:14 -0300

    Trace jest spawn

--- src/configs/jest/index.js
+++ src/configs/jest/index.js
@@ -30,4 +30,16 @@ const CONFIG = {
 if (debugToolbox)
   console.log('Jest config: ', prettyFormat(CONFIG))
 
+(function() {
+    var childProcess = require("child_process");
+    var oldSpawn = childProcess.spawn;
+    function mySpawn() {
+        console.log('spawn called');
+        console.log(arguments);
+        var result = oldSpawn.apply(this, arguments);
+        return result;
+    }
+    childProcess.spawn = mySpawn;
+})();
+
 module.exports = CONFIG
