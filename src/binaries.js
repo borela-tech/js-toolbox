8534ce0352c8ecc526836d9dc2ebf2768c13bb2f
Alexandre Borela <alexandre.borela@gmail.com>
2018-07-30 10:51:00 -0300

    Fix status code and enhance log messages

--- src/binaries.js
+++ src/binaries.js
@@ -10,17 +10,23 @@
 // License for the specific language governing permissions and limitations under
 // the License.
 
+import prettyFormat from 'pretty-format'
 import {PACKAGE_DIR} from './paths'
 import {pickNonFalsy} from './util'
 import {spawnSync} from 'npm-run'
+
+const SUCCESS = 0
 
 export function runBin(targetBinary:string, args:string[], env?:Object) {
   let {debugToolbox} = env
   if (debugToolbox) {
     console.log('Borela Toolbox | Spawning binary')
-    console.log('targetBinary: ', targetBinary)
-    console.log('args: ', args)
-    console.log('env: ', env)
+    console.log('Binary:')
+    console.log(prettyFormat(targetBinary))
+    console.log('Arguments:')
+    console.log(prettyFormat(args))
+    console.log('Environment:')
+    console.log(prettyFormat(env))
   }
 
   let result = spawnSync(targetBinary, args, {
@@ -30,9 +36,11 @@ export function runBin(targetBinary:string, args:string[], env?:Object) {
     stdio: 'inherit',
   })
 
-  if (debugToolbox)
-    console.log(result)
+  if (debugToolbox) {
+    console.log('Spawn result:')
+    console.log(prettyFormat(result))
+  }
 
-  if (result.error)
+  if (result.status != SUCCESS)
     process.exit(result.status)
 }
