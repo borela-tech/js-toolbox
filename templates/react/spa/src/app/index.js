09406eb2bd40450cfe40bc8338240c58581b5b29
Alexandre Borela <alexandre.borela@gmail.com>
2018-08-12 21:42:00 -0300

    Add basic spa template

---
+++ templates/react/spa/src/app/index.js
@@ -0,0 +1,18 @@
+// Copyright © 2016 Alexandre Borela <alexandre@borela.tech>
+//
+// NOTICE: All information contained herein is, and remains the property of
+// Alexandre Borela and its suppliers, if any. The intellectual and technical
+// concepts contained herein are proprietary to Alexandre Borela and its
+// suppliers and may be covered by patents, patents in process, and are
+// protected by trade secret or copyright law. Dissemination of this information
+// or reproduction of this material is strictly forbidden unless prior written
+// permission is obtained from Alexandre Borela.
+
+import React, { Component } from 'react'
+
+export default class App extends Component {
+  render() {
+    // throw Error('Test!')
+    return <div>BORELA!</div>
+  }
+}
