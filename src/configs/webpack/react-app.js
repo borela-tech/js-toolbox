392327360d24d8b24b8730ba2d3a14569000f539
Alexandre Borela <alexandre.borela@gmail.com>
2018-09-06 16:15:12 -0300

    Externalize react in spa

--- src/configs/webpack/react-app.js
+++ src/configs/webpack/react-app.js
@@ -72,6 +72,11 @@ export default function () {
   setEntryPoint(config)
   setHtmlTemplate(config)
 
+  config.externals = {
+    react: 'commonjs react',
+    'react-dom': 'commonjs react-dom',
+  }
+
   config.target = 'web'
   return [config]
 }
