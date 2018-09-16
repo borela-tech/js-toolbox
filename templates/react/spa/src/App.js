c4f02fa8376496bd8b36de7e07650e5dc69b58c6
Alexandre Borela <alexandre.borela@gmail.com>
2018-09-16 16:52:29 -0300

    Update SPA template for further testing

--- templates/react/spa/src/App.js
+++ templates/react/spa/src/App.js
@@ -10,11 +10,11 @@
 // License for the specific language governing permissions and limitations under
 // the License.
 
+import './App.css'
 import React, {Component} from 'react'
 
 export default class App extends Component {
   render() {
-    throw Error('Test!')
     return <div>BORELA!</div>
   }
 }
