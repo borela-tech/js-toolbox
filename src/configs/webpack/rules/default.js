da1bf2e70b7c1dc2005f38d69e186a1043b0cab7
Alexandre Borela <alexandre.borela@gmail.com>
2018-09-19 23:02:22 -0300

    Update asset loader path resolution

--- src/configs/webpack/rules/default.js
+++ src/configs/webpack/rules/default.js
@@ -10,17 +10,41 @@
 // License for the specific language governing permissions and limitations under
 // the License.
 
+import {
+  getProjectDir,
+  TOOLBOX_DIR,
+} from '../../../paths'
+
+import {isPathSubDirOf} from '../../../utils'
 import {relative, sep} from 'path'
-import {getProjectDir} from '../../../paths'
 
 const PROJECT_DIR = getProjectDir()
 
+/**
+ * Calculate the file path for the loaded asset.
+ */
 function fileName(file) {
-  const RELATIVE = relative(PROJECT_DIR, file)
+  let prefix = null
+  let reference = null
+
+  if (isPathSubDirOf(file, PROJECT_DIR)) {
+    // Assets from the target project.
+    prefix = []
+    reference = PROJECT_DIR
+  } else if (isPathSubDirOf(file, TOOLBOX_DIR)) {
+    // Assets from the Toolbox.
+    prefix = ['borela-js-toolbox']
+    reference = TOOLBOX_DIR
+  }
+
+  const RELATIVE = relative(reference, file)
   let nodes = RELATIVE.split(sep)
 
   if (nodes[0] === 'src')
     nodes.shift()
+
+  if (prefix.length > 0)
+    nodes = [...prefix, ...nodes]
 
   return `${nodes.join('/')}?[sha512:hash:base64:8]`
 }
