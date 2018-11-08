317aa3c3f2e0e76b168cbb4200d1f3f32ebfa921
Alexandre Borela <alexandre.borela@gmail.com>
2018-11-08 06:22:46 -0200

    Restore CSS rules

---
+++ src/configs/webpack/rules/css-module.js
@@ -0,0 +1,52 @@
+// Licensed under the Apache License, Version 2.0 (the “License”); you may not
+// use this file except in compliance with the License. You may obtain a copy of
+// the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an “AS IS” BASIS, WITHOUT
+// WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+// License for the specific language governing permissions and limitations under
+// the License.
+
+import MiniCssExtractPlugin from 'mini-css-extract-plugin'
+import pluginAutoPrefixer from 'autoprefixer'
+import store from '../../../state'
+
+export default function () {
+  let {
+    command,
+    options: {disableSourceMaps},
+  } = store.getState()
+
+  let rootLoader = 'style-loader'
+  let rootOptions = {sourceMap: !disableSourceMaps}
+
+  if (command !== 'serve') {
+    rootLoader = MiniCssExtractPlugin.loader
+    rootOptions = undefined
+  }
+
+  return {
+    test: /\.module\.css$/,
+    use: [{
+      loader: rootLoader,
+      options: rootOptions,
+    }, {
+      loader: 'css-loader',
+      options: {
+        importLoaders: 1,
+        localIdentName: '[local]---[name]---[hash:base64]',
+        modules: true,
+        sourceMap: !disableSourceMaps,
+      },
+    }, {
+      loader: 'postcss-loader',
+      options: {
+        plugins: [pluginAutoPrefixer],
+        sourceMap: !disableSourceMaps,
+      },
+    }],
+  }
+}
